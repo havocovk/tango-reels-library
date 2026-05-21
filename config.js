39144466d4bdeb502fdec7359b1a5975d85fe15b
Orhan Veli Kazanci <orhanveli.kazanci@gmail.com>
2026-05-21 21:51:16 +0300

    Update 03

--- config.js
+++
@@ -1,5 +0,0 @@
-
-// SUPABASE BAĞLANTI BİLGİLERİ
-export const SUPABASE_URL = "https://coasifpuxnykgswnjguh.supabase.co";
-export const SUPABASE_KEY = "sb_publishable_xQ58KgtPyiyCUHWynnHdbQ_k78iXfS1";
-
