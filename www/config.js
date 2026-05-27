cfa7ec2e86092e65474f2daffe9dcbeb84221fff
Orhan Veli Kazanci <orhanveli.kazanci@gmail.com>
2026-05-27 17:23:44 +0300

    Update30

--- www/config.js
+++ www/config.js
@@ -137,7 +137,7 @@ export const translations = {
             other: ""
         },
         platformIconUrls: {
-            drive: "./images/google-drive.png",
+            drive: "./images/google-drive-icon-2026.png",
             youtube: "./images/youtube-icon.png",
             instagram: "./images/instagram-icon-2022.png",
             facebook: "./images/facebook-icon-2023.png",
